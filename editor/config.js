47bcee528699da98cb0ed6a85b339542c77000de
Роман Теплов <a14440850@Romans-MacBook-Pro.local>
2020-10-16 14:40:03 +0300

    4.0.12

--- editor/config.js
+++ editor/config.js
@@ -2,7 +2,7 @@ window.sberCareChat.init({
     startForm: 'Bot', // 'Chat' | 'Icon' | 'Bot' | 'Elena' | 'Conversations'
     conversations: ['Bot'], //каждый должен быть описан в конфиге | 'Elena'
     applicationName: 'sberCareChat', // для вызова публичных методов
-    mainBundlePath: 'https://sbchat.netlify.app/dist/latest/',// путь до js файла
+    mainBundlePath: 'https://sbchat.netlify.app/dist/4.0.12/',// путь до js файла
     mountContainerId: 'retailChat-container', // id контейнера для рендера чата
     headerIsEnabled: true,
     chatEnabled: true,
@@ -16,6 +16,40 @@ window.sberCareChat.init({
             withPicture: false,
             title: 'Центр поддержки',
             text: 'С радостью ответим на ваши вопросы в чате! Если бот не сможет помочь – к чату подключиться оператор.'
+        },
+    },
+    Chat: {
+        type: 'messanger',
+        name: 'Сбербанк',
+        iconImgUrl: '', // если iconType: 'custom'
+        apiRestUrl: 'https://messenger.sberbank.ru/api/device/auth_prelogin', // IFT- messenger-ift.sberbank.ru, PSI- messenger-t.sberbank.ru PROM- messenger.sberbank.ru
+        apiWSUrl: 'wss://messenger.sberbank.ru/api/', // IFT- messenger-ift.sberbank.ru, PSI- messenger-t.sberbank.ru PROM- messenger.sberbank.ru
+        isNewOperatorPlace: false,
+        withAnimation: true,
+        firstMessage: {
+            text: 'Здравствуйте! Здесь вы можете задать общие вопросы по новой трубе',
+            suggestions: [{
+                text: 'Что нового?',
+                order_id: 0
+            }, {
+                text: 'Что это такое?',
+                order_id: 1
+            }]
+        },
+        domain: 'sberbank.ru', // по умолчанию location.host
+        browserName: '', // для отправки auth_prelogin
+        browserVersion: '', // для отправки auth_prelogin
+        chatVersion: '', // для отправки auth_prelogin
+        reconnectMaxCount: 2,
+        reconnectDuration: 10,
+        initTimeout: 30,
+        suggestionShowTime: 60,
+        dictionary: {
+            headerTitle: 'Чат Х',
+            botName: 'Помощник по чатам Х',
+            inputPlaceholder: 'Напишите сообщение...',
+            techBreakTitle: 'На данный момент отправка сообщений невозможна',
+            techBreakText: 'Попробуйте зайти позже',
         },
     },
     Bot: {
